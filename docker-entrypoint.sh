08311aedeada59fd635900a581d75629eb497ca7
nicoguaro20 <jungarciagu@unal.edu.co>
2025-10-28 12:14:19 -0500

    Ajuste para activar flujo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,17 +8,19 @@ if [ -d "/workflows" ]; then
   echo "📁 Carpeta /workflows encontrada. Importando archivos JSON..."
   
   for f in /workflows/*.json; do
-    # Evita error si no hay archivos
     [ -e "$f" ] || { echo "⚠️ No se encontraron archivos JSON en /workflows."; break; }
 
     echo "➡️ Importando workflow: $f"
     n8n import:workflow --input="$f" --separate --overwrite
   done
+
+  echo "✅ Importación completada. Activando workflows..."
+  n8n update:workflow --all --active=true
 else
   echo "⚠️ Carpeta /workflows no encontrada. Saltando importación."
 fi
 
-echo "✅ Importación completada. Iniciando n8n..."
+echo "✅ Workflows importados y activados. Iniciando n8n..."
 
 # Railway define PORT automáticamente, n8n lo usará si existe
 exec n8n start
